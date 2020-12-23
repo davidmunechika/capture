e7f5c8836d6467664c74cfb0db55471c72faca3b
davidmunechika <davidam1203@gmail.com>
2020-12-23 10:26:16 -0500

    Our work animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -45,14 +45,19 @@ const OurWork = () => {
       </Movie>
       <Movie ref={element} variants={fade} animate={controls} initial="hidden">
         <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
-      <Movie>
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial="hidden"
+      >
         <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="good-times">
           <img src={goodtimes} alt="goodtimes" />
         </Link>
